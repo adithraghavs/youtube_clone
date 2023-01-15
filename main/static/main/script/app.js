c627e0b0caae80550a065428a280bd49961518a4
adithraghavs <adithraghavs@gmail.com>
2023-01-15 11:05:37 +0530

    initial commit

---
+++ main/static/main/script/app.js
@@ -0,0 +1,34 @@
+var topnavSearchInput = document.querySelector("#topnavSearchInput");
+var topnavSearchOverlay = document.querySelectorAll(".topnav-search-overlay");
+var topnavSearchbarPackage = document.querySelector(".topnav-searchbar-package");
+var topnavSearchFocusIndicator = document.querySelector(".topnav-search-focus-indicator");
+var topnavSearchIconBtn = document.querySelector(".topnav-search-icon-btn");
+var chips = document.querySelector(".chips");
+var chips_left_btn = document.querySelector("left_btn");
+
+topnavSearchbarPackage.classList.remove("topnav-search-overlay");
+
+function topnavSearchInputOnFocusEvent() {
+    topnavSearchInput.style.borderTopLeftRadius = "0px";
+    topnavSearchInput.style.borderBottomLeftRadius = "0px";
+    topnavSearchInput.style.position = "relative";
+    topnavSearchInput.style.top = "-38px";
+    topnavSearchIconBtn.style.position = "relative";
+    topnavSearchIconBtn.style.top = "-31px";
+    topnavSearchFocusIndicator.style.display = "block";
+
+};
+
+function topnavSearchInputOnFocusOutEvent() {
+    topnavSearchInput.style.borderTopLeftRadius = "40px";
+    topnavSearchInput.style.borderBottomLeftRadius = "40px";
+    topnavSearchFocusIndicator.style.display = "none";
+    topnavSearchInput.style.position = "relative";
+    topnavSearchInput.style.top = "0px";
+    topnavSearchIconBtn.style.position = "relative";
+    topnavSearchIconBtn.style.top = "7px";
+};
+
+chips.addEventListener("scroll", () => {
+    chips_left_btn.style.display = "block;"
+});
